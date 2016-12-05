d9f0654e9c511addd1941353fc3adf3882ef7981
Olof Astrand (on Laptop) <olof.astrand@gmail.com>
2016-12-06 00:22:28 +0100

    Added telnet client and mqtt

--- main/awho.c
+++ main/awho.c
@@ -1,61 +1,141 @@
+
+
+#include <string.h>
 #include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
 #include "esp_system.h"
-#include "esp_event.h"
+#include "esp_wifi.h"
 #include "esp_event_loop.h"
+#include "esp_log.h"
 #include "nvs_flash.h"
 #include "driver/gpio.h"
-#include <string.h>
+
+
 #include "lwip/err.h"
 #include "lwip/sockets.h"
 #include "lwip/sys.h"
-#include "lwip/netif.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+
+
 #include "netif/etharp.h"
 #include "netif/etharp.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event_loop.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+#include "lwip/dns.h"
+#include "telnet.h"
+
+static const char *TAG = "awho";
+
 
 extern void echo_application_thread(void *pvParameters);
 
+extern void Task_lwip_init(void * pParam);
 
 //#include "cpu.h"
 // Change in lwipopts.h add 
 // #define ETHARP_DEBUG                    LWIP_DBG_ON
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
+/* FreeRTOS event group to signal when we are connected & ready to make a request */
+static EventGroupHandle_t wifi_event_group;
+
+/* The event group allows multiple bits for each event,
+   but we only care about one event - are we connected
+   to the AP with an IP? */
+const int CONNECTED_BIT = BIT0;
+
+
+static char tag[] = "telnet";
+
+static void recvData(uint8_t *buffer, size_t size) {
+	char responseMessage[100];
+	ESP_LOGD(tag, "We received: %.*s", size, buffer);
+	sprintf(responseMessage, "Thanks for %d bytes of data\n", size);
+	telnet_esp32_sendData((uint8_t *)responseMessage, strlen(responseMessage));
+}
+
+static void telnetTask(void *data) {
+	ESP_LOGD(tag, ">> telnetTask");
+	telnet_esp32_listenForClients(recvData);
+	ESP_LOGD(tag, "<< telnetTask");
+	vTaskDelete(NULL);
+}
+
+
+static esp_err_t event_handler(void *ctx, system_event_t *event)
 {
+    switch(event->event_id) {
+    case SYSTEM_EVENT_STA_START:
+        esp_wifi_connect();
+        break;
+    case SYSTEM_EVENT_STA_GOT_IP:
+        xEventGroupSetBits(wifi_event_group, CONNECTED_BIT);
+        xTaskCreatePinnedToCore(&telnetTask, "telnetTask", 8048, NULL, 5, NULL, 0);
+        break;
+    case SYSTEM_EVENT_STA_DISCONNECTED:
+        /* This is a workaround as ESP32 WiFi libs don't currently
+           auto-reassociate. */
+        esp_wifi_connect();
+        xEventGroupClearBits(wifi_event_group, CONNECTED_BIT);
+        break;
+    default:
+        break;
+    }
     return ESP_OK;
+}
+
+static void initialise_wifi(void)
+{
+   printf("Initiated\n");
+
+
+    tcpip_adapter_init();
+    printf("adapter_init\n");
+
+    wifi_event_group = xEventGroupCreate();
+    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
+    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+    wifi_config_t wifi_config = {
+        .sta = {
+            #include "secret.h"
+            //.ssid = "ssid",
+            //.password = "password",
+        },
+    };
+    ESP_LOGI(TAG, "Setting WiFi configuration SSID %s...", wifi_config.sta.ssid);
+    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK( esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config) );
+    ESP_ERROR_CHECK( esp_wifi_start() );
+    printf("Connected\n");
+
 }
 
 int app_main(void)
 {
-    // To ensure proper MMU setup, not needed
-    //cpu_configure_region_protection();
     nvs_flash_init();
-    //system_init();
-    printf("Initiated\n");
+    initialise_wifi();
+ 
+    //xTaskCreate(&echo_application_thread, "echo_thread", 2048, NULL, 12, NULL);
+    //xTaskCreatePinnedToCore(&telnetTask, "telnetTask", 8048, NULL, 5, NULL, 0);
 
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    // For qemu debugging
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-	   //#include "secret.h"
-	   .ssid = "ssid",
-	   .password = "password",
-	   .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
-    printf("Connected\n");
+ /* Configure the IOMUX register for pad BLINK_GPIO (some pads are
+       muxed to GPIO on reset already, but some default to other
+       functions and need to be switched to GPIO. Consult the
+       Technical Reference for a list of pads and their default
+       functions.)
+    */
+    gpio_pad_select_gpio(GPIO_NUM_5);
 
-    xTaskCreate(&echo_application_thread, "echo_thread", 2048, NULL, 12, NULL);
-
-    //gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+    gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
     int level = 0;
     // Send arp request
 
@@ -71,7 +151,6 @@ int app_main(void)
     ip4_addr_t *cacheaddr;
     struct eth_addr *cachemac;
 
-
     netif=netif_find("en0");
     if (!netif) {
         printf("No en0");
@@ -81,9 +160,9 @@ int app_main(void)
     char tmpBuff[20];
     while (true) {
 
-        sprintf(tmpBuff,"192.168.0.%d",hostnum);
+        sprintf(tmpBuff,"192.168.1.%d",hostnum);
 
-        IP4_ADDR(&scanaddr, 192, 168 , 0, hostnum);
+        IP4_ADDR(&scanaddr, 192, 168 , 1, hostnum);
 
         struct netif *chacheif=netif;
         for (int j=0;j<ARP_TABLE_SIZE;j++) {
@@ -103,18 +182,18 @@ int app_main(void)
             // To clean cache
 
 
-        //gpio_set_level(GPIO_NUM_4, level);
+        gpio_set_level(GPIO_NUM_5, level);
         if (netif)
         {
-            printf("ARP request %s\n",tmpBuff);
-            err_t ret=etharp_request(netif, &scanaddr);
+	      //printf("ARP request %s\n",tmpBuff);
+          err_t ret=etharp_request(netif, &scanaddr);
             if (ret<0) {
                 printf("Failed request %s\n",tmpBuff);
             }
         }
 
         level = !level;
-        vTaskDelay(10/portTICK_PERIOD_MS);
+        vTaskDelay(100/portTICK_PERIOD_MS);
         hostnum++;
     }
 
